a9e10ba0f2c6c570eac3fa90806763b4f313bcab
ImEphra06 <bart94620@hotmail.fr>
2025-06-02 13:49:38 +0200

    update of package

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,13 @@
 "use strict";
 
-var _react = _interopRequireDefault(require("react"));
-var _client = _interopRequireDefault(require("react-dom/client"));
-var _reactRouterDom = require("react-router-dom");
-require("./index.css");
-var _App = _interopRequireDefault(require("./App"));
-var _reportWebVitals = _interopRequireDefault(require("./reportWebVitals"));
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "Modal", {
+  enumerable: true,
+  get: function get() {
+    return _modal["default"];
+  }
+});
+var _modal = _interopRequireDefault(require("./modal/modal"));
 function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
-var root = _client["default"].createRoot(document.getElementById('root'));
-root.render(/*#__PURE__*/_react["default"].createElement(_react["default"].StrictMode, null, /*#__PURE__*/_react["default"].createElement(_reactRouterDom.BrowserRouter, null, /*#__PURE__*/_react["default"].createElement(_App["default"], null))));
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-(0, _reportWebVitals["default"])();
